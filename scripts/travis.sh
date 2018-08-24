72b57dfbdac202465bb3ecc1bc0911060becdcd4
Giovanni <me@giovanniterlingen.com>
2018-08-24 15:15:14 +0200

    Add initial coveralls commands

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -24,3 +24,13 @@ echo "Analyzing the extracted Dart libraries."
 ../flutter/bin/flutter analyze lib test
 
 ../flutter/bin/flutter test
+
+if [ "$COVERALLS_TOKEN" ]; then
+  echo "Found coveralls key. Starting coveralls process..."
+  ../flutter/bin/flutter packages pub global activate dart_coveralls
+  ../flutter/bin/flutter packages pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/json_parser_test.dart
+fi
